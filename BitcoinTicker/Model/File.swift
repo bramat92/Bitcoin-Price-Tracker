e9efde43734de267418506c629d26fc0e589f078
Bernhardt Ramat <bernhardt.rmt@gmail.com>
2018-10-15 03:43:52 -0600

    Removed skeleton owner’s name and company

--- BitcoinTicker/Model/File.swift
+++ BitcoinTicker/Model/File.swift
@@ -2,8 +2,7 @@
 //  File.swift
 //  BitcoinTicker
 //
-//  Created by Bernhardt Ramat on 8/28/18.
-//  Copyright © 2018 London App Brewery. All rights reserved.
+//  Created by Bernhardt Ramat on 8/28/18
 //
 
 import UIKit
